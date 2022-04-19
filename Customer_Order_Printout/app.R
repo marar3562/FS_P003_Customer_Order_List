314fb7c5c1c5676bddafee57bb520365e3d7d52c
marar3562 <rmargh@gmail.com>
2022-04-19 10:39:12 -0500

    Update Step 1
    Added in original logic for Step 1. Started on adding emojis to show when complete or errors have occurred. Need to keep updating Step 1 with emojis before going to Step 2.

--- Customer_Order_Printout/app.R
+++ Customer_Order_Printout/app.R
@@ -52,6 +52,14 @@ date_df = wed_thu_dates |>
             , by = c('date')
             ) 
 
+
+order_only_list = c('1 No Share - Order Only (even wk)',
+                    '1 No Share - Order Only (odd wk)',
+                    '1 No Produce - Order Only (even wk)',
+                    '1 No Produce - Order Only (odd wk)')
+
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -64,12 +72,13 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
       sidebarPanel(
-        h4("Step 1a"),
+        textOutput("step1a"),
         dateInput("date_value","Enter Pickup Date (used in printout)"
                   ,ymd(str_sub(lubridate::now(),1, 10))
                   ,datesdisabled = all_date$all_dates
                   ),
         actionButton("p_file","Step 1a. Process Date Selected"),
+        uiOutput("week_value"),
         h4("Step 1b"),
         uiOutput("file_rui"),
         uiOutput("button_1b_rui"),
@@ -78,18 +87,22 @@ ui <- fluidPage(
         
         ),
       mainPanel(
+        # tableOutput("mult_pickup_times_tbl"),
+        splitLayout(tableOutput("member_start_tbl")
+                    ,tableOutput("pickup_time_counts_tbl")),
         dataTableOutput("preview1")
       )
       )
-    )#,
-    # tabPanel(
-    #   sidebarLayout(
-    #     sidebarPanel(
-    #     ),
-    #     mainPanel(
-    #     )
-    #   )
-    # )
+    ),
+    tabPanel("Step 2 - Standard Shares",
+      sidebarLayout(
+        sidebarPanel(
+          h4("Step 2a")
+        ),
+        mainPanel(
+        )
+      )
+    )
     )
 )
 
@@ -98,9 +111,19 @@ server <- function(input, output) {
 
   ## Step 1 (Load Data)
   
+  output$step1a = renderText(paste0("Step 1a ",emo::ji('black_circle')))
+  
   ### Load Data
   observeEvent(input$p_file,{
     if(input$date_value %in% wed_thu_dates$date_all) {
+      date_df_fltr = date_df |> 
+        filter(date_all == input$date_value) |> 
+        select(season, week, date_all) |> 
+        distinct()
+      
+      output$week_value <- renderUI(
+        h6(paste0("Week Number: ",date_df_fltr$week," Season: ",date_df_fltr$season))
+      )
       output$file_rui <- renderUI(
         fileInput("file_input", "Choose your file in csv"
                   , multiple = FALSE
@@ -109,6 +132,7 @@ server <- function(input, output) {
       output$button_1b_rui <- renderUI(
         actionButton("pp_file","Step 1b. Check File Format"),
       )
+      output$step1a = renderText(paste0("Step 1a ",emo::ji('heavy_check_mark')))
     } else {
       showModal(modalDialog(
         title = "Warning",
@@ -120,6 +144,8 @@ server <- function(input, output) {
   })
   
   df_format = reactiveValues(data = NULL)
+  member_start = reactiveValues(data = NULL) #use later for member numbers before and after
+  df_long_tm = reactiveValues(data = NULL)   #passed on as data set for steps
   
   observeEvent(input$pp_file,{
     if(is.null(input$file_input$datapath)){
@@ -187,20 +213,158 @@ server <- function(input, output) {
         easyClose = TRUE
       ))
     } else {
-      output$preview1 <- renderDataTable(datatable(df_orig))
+      
+      #### Member Count - Start
+      df_orig_sh = df_orig  |>  
+        left_join(sh_shares |> #bringing in short group name
+                    select(group_name, group_name_short) |> 
+                    distinct()
+                  , by = c('group_name')
+        ) |> 
+        mutate(group_name = ifelse(is.na(group_name_short) 
+                                   | group_name_short == '', group_name, group_name_short)
+        ) |>
+        select(-group_name_short)
+      
+      sh_shares_shrt = sh_shares |> 
+        mutate(group_name = ifelse(is.na(group_name_short) 
+                                   | group_name_short == '', group_name, group_name_short)
+        ) |>
+        select(-group_name_short)
+      
+      member_start$data = df_orig_sh |> 
+        select(group_name, firstname, lastname) |> 
+        distinct() |> 
+        group_by(group_name) |> 
+        summarise(n_start = n())
+      
+      member_start_view = member_start$data |> 
+        rename('Group Name' = group_name, 'Member Count' = n_start)
+      
+      # remove("preview1") #struggling to find how to easily remove the first table 
+      output$member_start_tbl <- renderTable(member_start_view)
+      
+      #### Make data long
+      
+      df_orig_long = df_orig_sh  |> 
+        mutate(order = strsplit(as.character(order), ",")) |>
+        unnest(order) |> 
+        mutate(pickuptime = ifelse(str_detect(order, "Pickup Time"), 2, NA),
+               order = trimws(order),
+               item_original = order
+        ) |> 
+        separate(order, into = c('quantity','item'), sep = 'x', extra = "merge") |> 
+        mutate(item = ifelse(is.na(item) & !is.na(quantity), quantity, item)
+               ,quantity = ifelse(item == quantity, NA, quantity)
+               ,group_bool = ifelse(is.na(quantity) & !is.na(item) & !item %in% order_only_list, 1, NA)
+               ,rank = ifelse(!is.na(group_bool), group_bool,
+                              ifelse(!is.na(pickuptime), pickuptime, 3))
+               ,name_long = paste0(lastname,', ',firstname,' [',id,']')
+        ) |> 
+        select(-group_bool, -pickuptime) |> 
+        rbind(df_orig_sh |> #bringing in a new row for each group_name
+                select(group_name) |> 
+                distinct() |> 
+                mutate(firstname = '0'
+                       ,lastname = '0'
+                       ,quantity = NA
+                       ,item = group_name
+                       ,id = 0
+                       ,item_original = group_name
+                       ,rank = -1
+                       ,name_long = paste0(lastname,', ',firstname,' [',id,']')
+                )
+        ) |> 
+        rbind(df_orig_sh |> #bringing in a new row for each member name
+                select(group_name, firstname, lastname, id) |> 
+                distinct() |> 
+                mutate(quantity = NA
+                       ,item = paste0(lastname,', ',firstname)
+                       ,rank = 0
+                       ,item_original = item
+                       ,name_long = paste0(lastname,', ',firstname,' [',id,']')
+                ) |> 
+                select(group_name, firstname, lastname, quantity, item, id, item_original, rank, name_long)
+        ) |> 
+        rbind(df_orig_sh |> #bringing in a new spacer row for each member name
+                select(group_name, firstname, lastname, id) |> 
+                distinct() |> 
+                mutate(quantity = NA
+                       ,item = NA
+                       ,rank = 4
+                       ,item_original = NA
+                       ,name_long = paste0(lastname,', ',firstname,' [',id,']')
+                ) |> 
+                select(group_name, firstname, lastname, quantity, item, id, item_original, rank, name_long)
+        ) |> 
+        arrange(group_name, lastname, firstname, id, rank) 
+      
+      #### Pickup Times found
+      
+      pickup_time = df_orig_long |> 
+        filter(rank == 2) |> 
+        group_by(Time = item) |> 
+        summarise(count = n()) |> 
+        ungroup() |> 
+        mutate(val = Time) |> 
+        separate(val, into = c('q','time','description'), sep = ' ', extra = "merge") |> 
+        arrange(description, time, q) 
+      
+      pickup_time_counts = pickup_time |> 
+        select('Pickup Time' = Time, 'Member Count' = count)
+      
+      df_long_tm_df = df_orig_long |> 
+        left_join(
+          df_orig_long |> 
+            filter(rank == 2) |> 
+            select(id, item) |> 
+            distinct() |> 
+            left_join(pickup_time |> 
+                        select(item = Time, description, time)
+                      , by = c('item')
+            ) |>                                #accounts for if a member accidentally puts in two pickup times
+            arrange(id, description, time) |>   # look at first based on description and pickup time
+            group_by(id) |> 
+            mutate(count = rank(row_number())) |> 
+            filter(count == 1) |> 
+            rename(time_descr = item) |> 
+            select(-count) |> 
+            ungroup()
+          , by = c('id')
+        ) |> 
+        mutate(description = ifelse(rank == -1, '.....', description)) |> 
+        arrange(group_name, description, time, lastname, firstname, id, rank) 
+      
+      # mult_pickup_times = df_long_tm_df |> 
+      #   filter(rank == 2) |> 
+      #   group_by(name_long) |> 
+      #   mutate(count = sum(n())) |> 
+      #   filter(count > 1) |> 
+      #   select(name_long, item, count) |> 
+      #   distinct()
+      
+      df_long_tm_view = df_long_tm_df |>
+        select('Group Name' = group_name, 'First Name' = firstname
+               , 'Last Name' = lastname, 'Description' = item_original)
+      
+      df_long_tm$data = df_long_tm_df
+      
+      # if (dim(mult_pickup_times)[1] > 0) {
+      #   output$mult_pickup_times_tbl <- renderTable(mult_pickup_times)
+      #   output$pickup_time_counts_tbl <- renderTable(pickup_time_counts)
+      #   output$preview1 <- renderDataTable(datatable(df_long_tm_view))
+      #   
+      # } else {
+        output$pickup_time_counts_tbl <- renderTable(pickup_time_counts)
+        output$preview1 <- renderDataTable(datatable(df_long_tm_view))
+      # }
+      
+      
     }
     
   })
-  
-  ######ALL THREE OF THESE BELOW STILL NEED TO BE BUILT INTO THE APP ABOVE!!!
-  #### Member Count - Start
-  
-  #### Make data long
-  
-  #### Pickup Times found
-  
-  
-  
+
+  #########CURRENTLY IN THE PROCESS OF IMPLEMENTING EMOJIS TO SHOW WHERE WORK IS COMPLETE OR ERRORS HAVE OCCURED
   
   ## Step 2 (Standard Shares)
   
